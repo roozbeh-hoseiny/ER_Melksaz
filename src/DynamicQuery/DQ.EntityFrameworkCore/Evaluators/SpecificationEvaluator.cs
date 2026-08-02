a9d64673c0ff36db789b2b67f75080c38fdd512c
roozbeh hosseiny <roozbeh.hoseiny@dpi.ir>
2026-08-02 09:29:58 +0330

    SQ.EntityFrameworkCore added

---
+++ src/DynamicQuery/DQ.EntityFrameworkCore/Evaluators/SpecificationEvaluator.cs
@@ -0,0 +1,45 @@
+﻿using DQ.Abstraction.Specifications;
+
+namespace DQ.EntityFrameworkCore.Evaluators;
+
+/// <summary>
+/// Applies specifications to Entity Framework Core queries.
+/// </summary>
+/// <remarks>
+/// This evaluator converts a specification into an IQueryable pipeline.
+/// The evaluator does not execute the query. Execution remains the
+/// responsibility of Entity Framework Core through methods such as
+/// ToListAsync, FirstAsync, CountAsync, etc.
+/// </remarks>
+public static class SpecificationEvaluator
+{
+    /// <summary>
+    /// Applies the specified specification to the query.
+    /// </summary>
+    /// <typeparam name="TEntity">
+    /// The entity type.
+    /// </typeparam>
+    /// <param name="query">
+    /// The source query.
+    /// </param>
+    /// <param name="specification">
+    /// The specification containing filtering criteria.
+    /// </param>
+    /// <returns>
+    /// An IQueryable with the specification applied.
+    /// </returns>
+    public static IQueryable<TEntity> Apply<TEntity>(
+        IQueryable<TEntity> query,
+        ISpecification<TEntity>? specification)
+        where TEntity : class
+    {
+        ArgumentNullException.ThrowIfNull(query);
+
+        if (specification?.Criteria is null)
+        {
+            return query;
+        }
+
+        return query.Where(specification.Criteria);
+    }
+}
---
+++ src/DynamicQuery/DQ.EntityFrameworkCore/Extensions/IQueryableSpecificationExtensions.cs
@@ -0,0 +1,33 @@
+﻿using DQ.Abstraction.Specifications;
+using DQ.EntityFrameworkCore.Evaluators;
+
+namespace DQ.EntityFrameworkCore.Extensions;
+
+/// <summary>
+/// Provides extension methods for applying specifications to IQueryable.
+/// </summary>
+public static class IQueryableSpecificationExtensions
+{
+    /// <summary>
+    /// Applies a specification to an Entity Framework Core query.
+    /// </summary>
+    /// <typeparam name="TEntity">
+    /// The entity type.
+    /// </typeparam>
+    /// <param name="query">
+    /// The source query.
+    /// </param>
+    /// <param name="specification">
+    /// The specification to apply.
+    /// </param>
+    /// <returns>
+    /// A new query with the specification criteria applied.
+    /// </returns>
+    public static IQueryable<TEntity> ApplySpecification<TEntity>(
+        this IQueryable<TEntity> query,
+        ISpecification<TEntity>? specification)
+        where TEntity : class
+    {
+        return SpecificationEvaluator.Apply(query, specification);
+    }
+}
