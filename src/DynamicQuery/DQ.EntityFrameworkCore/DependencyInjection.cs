2f52b0e47d29f38b602e39392308ca17d186452e
roozbeh hosseiny <roozbeh.hoseiny@dpi.ir>
2026-08-05 18:16:18 +0330

    developing

---
+++ src/DynamicQuery/DQ.EntityFrameworkCore/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using DQ.EntityFrameworkCore.Evaluators;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DQ.EntityFrameworkCore;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDynamicQueryEntityFrameworkCore(
+        this IServiceCollection services)
+    {
+        services.AddScoped<ISpecificationEvaluator, SpecificationEvaluator>();
+
+        services.AddScoped<ISpecificationPartEvaluator, CriteriaEvaluator>();
+        services.AddScoped<ISpecificationPartEvaluator, IncludeEvaluator>();
+        services.AddScoped<ISpecificationPartEvaluator, OrderEvaluator>();
+        services.AddScoped<ISpecificationPartEvaluator, PagingEvaluator>();
+        services.AddScoped<ISpecificationPartEvaluator, AsNoTrackingEvaluator>();
+        services.AddScoped<ISpecificationPartEvaluator, AsNoTrackingWithIdentityResolutionEvaluator>();
+        services.AddScoped<ISpecificationPartEvaluator, SplitQueryEvaluator>();
+
+        return services;
+    }
+}
---
+++ src/DynamicQuery/DQ.EntityFrameworkCore/Evaluators/PagingEvaluator.cs
@@ -0,0 +1,27 @@
+﻿using DQ.Abstraction.Specifications;
+
+namespace DQ.EntityFrameworkCore.Evaluators;
+
+public sealed class PagingEvaluator : ISpecificationPartEvaluator
+{
+    public IQueryable<TEntity> Apply<TEntity>(
+        IQueryable<TEntity> query,
+        ISpecification<TEntity> specification)
+        where TEntity : class
+    {
+        ArgumentNullException.ThrowIfNull(query);
+        ArgumentNullException.ThrowIfNull(specification);
+
+        if (specification.Skip.HasValue)
+        {
+            query = query.Skip(specification.Skip.Value);
+        }
+
+        if (specification.Take.HasValue)
+        {
+            query = query.Take(specification.Take.Value);
+        }
+
+        return query;
+    }
+}
